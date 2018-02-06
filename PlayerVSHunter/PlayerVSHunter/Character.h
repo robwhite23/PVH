be923158fd1dc736fbeb9cd1d2e734487c9c694c
robwhite23 <robwhite23@hotmail.com>
2018-02-06 17:19:36 +0000

    Created seperate source and header files for the character class

---
+++ PlayerVSHunter/PlayerVSHunter/Character.h
@@ -0,0 +1,41 @@
+#pragma once
+
+typedef struct {
+	char x;
+	char y;
+}Board_Pos;
+
+class character{
+public:
+	character(Board_Pos starting_pos, char icon) : current_pos(starting_pos), Board_Icon(icon){};
+	void SetIcon(char icon);
+	char GetIcon(void) const;
+	Board_Pos GetBoard_Pos(void) const;
+	void MoveRandom(void);
+	void MoveLeft(void);
+	void MoveRight(void);
+	void MoveUp(void);
+	void MoveDown(void);
+private:
+
+	char Board_Icon;
+	Board_Pos current_pos;
+};
+
+class player : public character{
+public:
+	player(Board_Pos starting_pos, char icon) : character(starting_pos, icon){};
+
+
+private:
+	const bool CPU_controlled = false;
+};
+
+class Hunter : public character{
+public:
+	Hunter(Board_Pos starting_pos, char icon) : Hunter(starting_pos, icon){};
+
+
+private:
+	const bool CPU_controlled = true;
+};
